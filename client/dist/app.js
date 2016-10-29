cb121223cbca985308502c9c3b46b0332cd3a561
ADMINISTRATOR\Administrator <sangnew2016@gmail.com>
2016-10-29 14:55:12 +0700

    Updated: rename function

--- client/dist/app.js
+++ client/dist/app.js
@@ -1019,6 +1019,7 @@
             rowClass: 'row-shop-owner',
             sortingClass: 'sortableTable',
 
+            defaultId: 0,
             defaultStatusId: 1,
             model: {
                 id: 'Id',
@@ -1035,75 +1036,80 @@
         init();
 
         function init() {
-            loadGridShopOwnerByData();
-            registerOnSelectedShopOwner();
+            loadGridByData();
+            registerOnSelectedRow();
             registerOnActionCommand();
         }
 
-        function loadGridShopOwnerByData() {
+        function loadGridByData() {
             Metis.getData(config.getListApi).then(function (data) {
                 showEditArea(false);
-                $('#' + config.gridId).html(loadGridShopOwner(data));
+                $('#' + config.gridId).html(buildHtmlGrid(data));
                 $('.' + config.sortingClass).tablesorter();
                 $('#' + config.tableId).dataTable({});
             });
-        }
 
-        function loadGridShopOwner(data) {
-            var str = "";
-            str += '<table id="' + config.tableId + '" class="table table-bordered table-hover">';
-            str += '  <thead>';
-            str += '    <tr>';
-            str += '      <th>#</th>';
-            str += '      <th>Full Name</th>';
-            str += '      <th>Phone</th>';
-            str += '      <th>Email</th>';
-            str += '      <th>Status</th> ';
-            str += '    </tr>';
-            str += '  </thead>';
-            str += '  <tbody>';
-            for (var i = 0; i < data.length; i++) {
-                var shopOwner = data[i];
-                str += '    <tr class="' + config.rowClass + '" id="' + shopOwner[config.model.id] + '">';
-                str += '      <td>' + shopOwner[config.model.id] + '</td>';
-                str += '      <td>' + shopOwner[config.model.fullName] + '</td>';
-                str += '      <td>' + shopOwner[config.model.phone] + '</td>';
-                str += '      <td>' + shopOwner[config.model.email] + '</td>';
-                str += '      <td>' + shopOwner[config.model.status] + '</td>';
+            function buildHtmlGrid(data) {
+                var str = "";
+                str += '<table id="' + config.tableId + '" class="table table-bordered table-hover">';
+                str += '  <thead>';
+                str += '    <tr>';
+                str += '      <th>#</th>';
+                str += '      <th>Full Name</th>';
+                str += '      <th>Phone</th>';
+                str += '      <th>Email</th>';
+                str += '      <th>Status</th> ';
                 str += '    </tr>';
+                str += '  </thead>';
+                str += '  <tbody>';
+                for (var i = 0; i < data.length; i++) {
+                    var shopOwner = data[i];
+                    str += '    <tr class="' + config.rowClass + '" id="' + shopOwner[config.model.id] + '">';
+                    str += '      <td>' + shopOwner[config.model.id] + '</td>';
+                    str += '      <td>' + shopOwner[config.model.fullName] + '</td>';
+                    str += '      <td>' + shopOwner[config.model.phone] + '</td>';
+                    str += '      <td>' + shopOwner[config.model.email] + '</td>';
+                    str += '      <td>' + shopOwner[config.model.status] + '</td>';
+                    str += '    </tr>';
+                }
+                str += '  </tbody>';
+                str += '</table>';
+                return str;
             }
-            str += '  </tbody>';
-            str += '</table>';
-            return str;
         }
 
-        function loadShopOwner(data) {
-            fillValueInputControls(data);
-            showEditArea(true);
-            $('#' + config.cancelBtnId).text('Cancel Editing');
-        }
+        function registerOnSelectedRow() {
+            $('#' + config.gridId).on('click', '.' + config.rowClass, onSelectRow);
 
-        function registerOnSelectedShopOwner() {
-            $('#' + config.gridId).on('click', '.' + config.rowClass, function (event) {
+            function onSelectRow() {
+                changeBackgroundOnSelectedRow();
+                config.defaultId = Number(this.getAttribute("id"));
+                Metis.getData(config.getDetailApi + config.defaultId).then(function (rows) {
+                    var rowData = rows && rows[0] || {};
+                    fillDataIntoDetailUI(rowData);
+                });
+            }
+
+            function changeBackgroundOnSelectedRow() {
                 $('.' + config.rowClass).css({ backgroundColor: '#fff' });
                 $(this).css({ backgroundColor: '#ddd' });
+            }
 
-                var shopOwnerId = Number(this.getAttribute("id"));
-                Metis.OwnerShop.Id = shopOwnerId;
-                Metis.getData(config.getDetailApi + shopOwnerId).then(function (shopOwner) {
-                    shopOwner = shopOwner && shopOwner[0] || {};
-                    loadShopOwner(shopOwner);
-                });
-            });
+            function fillDataIntoDetailUI(data) {
+                fillValueInputControls(data);
+                showEditArea(true);
+                $('#' + config.cancelBtnId).text('Cancel Editing');
+            }
         }
 
         function registerOnActionCommand() {
             $('#' + config.addBtnId).on('click', function (event) {
                 fillValueInputControls({});
                 showEditArea(true);
-                Metis.OwnerShop.Id = 0;
+                config.defaultId = 0;
                 $('#' + config.cancelBtnId).text('Cancel Adding');
             });
+
             $('#' + config.cancelBtnId).on('click', function (event) {
                 showEditArea(false);
             });
@@ -1120,12 +1126,12 @@
         function showEditArea(isShowed) {
             if (isShowed) {
                 $('#' + config.editAreaId).show(100, function () {
-                    $('html, body').animate({ scrollTop: $('#' + config.editAreaId).height() - 50 }, 1000);
+                    //    $('html, body').animate({ scrollTop: $('#' + config.editAreaId).height() - 50 }, 1000);
                 });
                 $('#' + config.cancelBtnId).show();
             } else {
                 $('#' + config.editAreaId).hide(100, function () {
-                    $('html, body').scrollTop();
+                    //    $('html, body').scrollTop();    
                 });
                 $('#' + config.cancelBtnId).hide();
             }
@@ -1135,7 +1141,7 @@
         // UPDATE & INSERT
         //======================================
 
-        $('#dev-shop-Owner-validate').validate({
+        $('#' + config.formValidate).validate({
             rules: setRule(),
             errorClass: 'help-block',
             errorElement: 'span',
@@ -1166,24 +1172,24 @@
         }
 
         function onSubmitHandler(form) {
-            var postedData = getPostedShopOwnerInput();
+            var postedData = getParameters();
 
-            if (Metis.OwnerShop.Id) {
-                Metis.putData("admin/putShopOwner", postedData).then(function (data) {
-                    loadGridShopOwnerByData();
+            if (config.defaultId) {
+                Metis.putData(config.putDetailApi, postedData).then(function (data) {
+                    loadGridByData();
                     showEditArea(false);
                 });
             } else {
-                Metis.postData("admin/postShopOwner", postedData).then(function (data) {
-                    loadGridShopOwnerByData();
+                Metis.postData(config.postDetailApi, postedData).then(function (data) {
+                    loadGridByData();
                     showEditArea(false);
                 });
             }
             return false; // blocks regular submit since you have ajax
 
-            function getPostedShopOwnerInput() {
+            function getParameters() {
                 var param = {};
-                param[config.model.id] = Metis.OwnerShop.Id || 0, param[config.model.fullName] = document.getElementById(config.model.fullName).value, param[config.model.phone] = document.getElementById(config.model.phone).value, param[config.model.email] = document.getElementById(config.model.email).value, param[config.model.status] = config.defaultStatusId, param[config.model.password] = document.getElementById(config.model.password).value, param[config.model.confirmPassword] = document.getElementById(config.model.confirmPassword).value;
+                param[config.model.id] = config.defaultId || 0, param[config.model.fullName] = document.getElementById(config.model.fullName).value, param[config.model.phone] = document.getElementById(config.model.phone).value, param[config.model.email] = document.getElementById(config.model.email).value, param[config.model.status] = config.defaultStatusId, param[config.model.password] = document.getElementById(config.model.password).value, param[config.model.confirmPassword] = document.getElementById(config.model.confirmPassword).value;
 
                 return param;
             }
